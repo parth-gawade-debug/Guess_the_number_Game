78d2cd1a9b7ea6e93d410ace8c19aa1c412f79ec
Parth Gawade <gawadeparth777@gmail.com>
2026-02-14 09:53:24 +0000

    Add initial implementation of the Guess the Number game

---
+++ initial.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int random_number=0;
+    int guess=0;
+    int maximum_number_of_guesses=5;
+    time_t t;
+     // Initialization of the random number generator
+     srand ((unsigned) time(&t));
+
+     // get the random number
+     
+}
