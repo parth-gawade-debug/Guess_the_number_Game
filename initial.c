dc191b17536dcb283b3511e982517630fb98eb80
Parth Gawade <gawadeparth777@gmail.com>
2026-02-14 10:17:12 +0000

    Enhance game logic and add build configuration for C/C++ project

--- initial.c
+++ initial.c
@@ -5,11 +5,41 @@
 int main() {
     int random_number=0;
     int guess=0;
-    int maximum_number_of_guesses=5;
+    int no_of_guesses;
     time_t t;
      // Initialization of the random number generator
      srand ((unsigned) time(&t));
 
      // get the random number
-     
+     random_number =rand() % 21;
+     printf("Welcome to the Guessing Game!\n");
+     printf("I have selected a random number between 0 and 20. Can you guess it?\n");
+
+     for (no_of_guesses = 5; no_of_guesses >0; no_of_guesses--) {
+        printf("You have %d guesses left.\n", no_of_guesses);
+        printf("Enter your guess: ");
+        scanf("%d", &guess);
+        if (guess == random_number) {
+            printf("Congratulations! You guessed the number correctly!\n");
+            break;
+        }
+        else if(guess <random_number) {
+            printf("Sorry wrong guess! The number is higher than %d.\n", guess);
+            break;
+        }
+        else if(guess > random_number) {
+            printf("Sorry wrong guess! The number is lower than %d.\n", guess);
+            break;
+        }
+        else {
+            printf("Invalid input! Please enter a number between 0 and 20.\n");
+        }
+        
+     }
+     if (no_of_guesses == 0) {
+        printf("Game over! You have used all your guesses. The number was %d.\n", random_number);
+     }
+        return 0;
+        
+
 }
